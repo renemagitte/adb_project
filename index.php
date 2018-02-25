47fc419d9d209a2861096f52214fa54fe13bff13
Ida <ida.bergstrom@medieinstitutet.se>
2018-02-25 10:22:45 +0100

    Setting up repository/first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require 'partials/session.php';
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php
+require 'head.php';
+require 'partials/database.php';
+require 'partials/print_posts.php';
+require 'partials/index_statements.php'; 
+?>
+
+<body>
+
+    
+
+ 
+   
+    <?php
+        require "footer.php";
+    ?>
+    
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ partials/database.php
@@ -0,0 +1,7 @@
+<?php
+
+$pdo = new PDO(
+  "mysql:host=idabergstrom.se;dbname=idabergs_adb;charset=utf8",
+  "idabergs_admin",
+  "hejhej123"
+);
